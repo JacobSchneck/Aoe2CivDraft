f975900c2b83c8a4b1d68cd7bd86de0822ef5420
Jacob Schneck <jschneck@Gerald.localdomain>
2021-01-11 18:09:17 -0600

    commiting changes to index.js and index.css hopefully

---
+++ civ-draft/src/index.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const CIVS = ['Aztecs', 'Berbers', 'Britons', 'Bulgarians', 'Burmese', 'Byzayntines', 'Celts', 
+        'Chinese', 'Cumans', 'Ethiopians', 'Franks', 'Goths',' Huns', 'Incas', 
+        'Indians', 'Italians', 'Japanese', 'Khmer', 'Koreans', 'Lithuanians', 'Magyars', 
+        'Malay', 'Malians', 'Mayans', 'Mongols', 'Persians', 'Portuguese', 'Saracens', 
+        'Slavs', 'Spanish', 'Tatars', 'Teutons', 'Turks', 'Vietnamese', 'Vikings']; 
+
+function CivBoardButton(props){
+    return (
+        <button className="civ-button" onClick={props.onClick}>
+            {props.name}
+        </button>
+    )
+}
+
+class CivBoard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            civButtons: Array(CIVS.length).fill(false),
+            labels: CIVS, 
+        };
+    }
+
+    handleClick(i) {
+        const civButtons = this.state.civButtons.slice()
+        civButtons[i] = true;
+        this.setState({
+            civButtons: civButtons,
+        });
+        return console.log(civButtons[i] + " " + this.state.labels[i]);
+    }
+
+    renderCivBoardButton(i) {
+        return (
+            <CivBoardButton
+            value={this.state.civButtons[i]}
+            name={this.state.labels[i]}  
+            onClick = {() => this.handleClick(i)}
+            />
+        );
+    }
+    render() {
+        return (  
+            <div>
+                <div className="civ-board-row">
+                    {this.renderCivBoardButton(0)}
+                    {this.renderCivBoardButton(1)}
+                    {this.renderCivBoardButton(2)}
+                    {this.renderCivBoardButton(3)}
+                    {this.renderCivBoardButton(4)}
+                    {this.renderCivBoardButton(5)}
+                    {this.renderCivBoardButton(6)}
+                </div>
+                <div className="civ-board-row">
+                    {this.renderCivBoardButton(7)}
+                    {this.renderCivBoardButton(8)}
+                    {this.renderCivBoardButton(9)}
+                    {this.renderCivBoardButton(10)}
+                    {this.renderCivBoardButton(11)}
+                    {this.renderCivBoardButton(12)}
+                    {this.renderCivBoardButton(13)}
+                </div>
+                <div className="civ-board-row">
+                    {this.renderCivBoardButton(14)}
+                    {this.renderCivBoardButton(15)}
+                    {this.renderCivBoardButton(16)}
+                    {this.renderCivBoardButton(17)}
+                    {this.renderCivBoardButton(18)}
+                    {this.renderCivBoardButton(19)}
+                    {this.renderCivBoardButton(20)}
+                </div>
+                <div className="civ-board-row">
+                    {this.renderCivBoardButton(21)}
+                    {this.renderCivBoardButton(22)}
+                    {this.renderCivBoardButton(23)}
+                    {this.renderCivBoardButton(24)}
+                    {this.renderCivBoardButton(25)}
+                    {this.renderCivBoardButton(26)}
+                    {this.renderCivBoardButton(27)}
+                </div>
+                <div className="civ-board-row">
+                    {this.renderCivBoardButton(28)}
+                    {this.renderCivBoardButton(29)}
+                    {this.renderCivBoardButton(30)}
+                    {this.renderCivBoardButton(31)}
+                    {this.renderCivBoardButton(32)}
+                    {this.renderCivBoardButton(33)}
+                    {this.renderCivBoardButton(34)}
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<CivBoard />, document.getElementById('root'));
